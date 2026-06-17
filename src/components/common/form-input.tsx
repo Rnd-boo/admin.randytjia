3b8c30ae43499007c0615dec1f4182d4480fe38e
Randy <rand4035@gmail.com>
2026-06-17 15:55:39 +0700

    feat: reusable form input

---
+++ src/components/common/form-input.tsx
@@ -0,0 +1,53 @@
+import { Controller, FieldValues, Path, UseFormReturn } from "react-hook-form";
+import { Field, FieldDescription, FieldError, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
+
+export default function FormInput<T extends FieldValues>({
+  form,
+  label,
+  name,
+  placeholder,
+  type = "text",
+  disabled = false,
+  className,
+  description,
+  isLoading,
+  readOnly = false,
+  required,
+  onChange,
+}: {
+  form: UseFormReturn<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: string;
+  disabled?: boolean;
+  className?: string;
+  description?: string;
+  isLoading?: boolean;
+  readOnly?: boolean;
+  required?: boolean;
+  onChange?: (value: string) => void;
+}) {
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+          <Input
+            {...field}
+            id={field.name}
+            aria-invalid={fieldState.invalid}
+            placeholder={placeholder}
+            autoComplete="off"
+            type={type}
+          />
+          <FieldDescription>{description}</FieldDescription>
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
+      )}
+    />
+  );
+}
